246f4d2c72161ca6e4c9dc4194952ebcc4d07c14
Ceronio Justin BIT <justin.ceronio@bit.admin.ch>
2024-11-18 22:13:16 +0100

    feat: add map functionality

--- src/pages/_app.ts
+++ src/pages/_app.ts
@@ -4,8 +4,6 @@ import { MotionPlugin } from '@vueuse/motion'
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-
-
 export default (app: App) => {
     app.use(MotionPlugin);
     app.component('font-awesome-icon', FontAwesomeIcon);
