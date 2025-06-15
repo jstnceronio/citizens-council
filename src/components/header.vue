028ebb0a0b262fc2648bf7cd178cb9601a4e8a49
Ceronio Justin BIT <justin.ceronio@bit.admin.ch>
2025-06-15 14:53:31 +0200

    chore(codebase): cleanup

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,188 +1,170 @@
 <template>
-    <div class="upper-header">
-        <div class="header-text">
-            <!-- Logo -->
-            <img src="/logo.png" class="header-logo">
+  <div class="upper-header">
+    <div class="header-text">
+      <!-- Logo -->
+      <img src="/logo.png" class="header-logo" />
 
-            <!-- Title -->
-            <div class="header-title">
-                <h1 class="header-title-main">DÄRLIGEN</h1>
-                <p class="header-title-secondary">Burgergemeinde am Thunersee</p>
-            </div>
-        </div>
-        
-        <!-- Mobile menu button -->
-        <button class="mobile-menu-button" @click="toggle()">
-            <div class="space-y-1">
-                <span ref="upper" class="burger-line"></span>
-                <span ref="middle" class="burger-line"></span>
-                <span ref="lower" class="burger-line"></span>
-            </div>
-            <span class="font-medium ml-4">Menu</span>
-        </button>
+      <!-- Title -->
+      <div class="header-title">
+        <h1 class="header-title-main">DÄRLIGEN</h1>
+        <p class="header-title-secondary">Burgergemeinde am Thunersee</p>
+      </div>
     </div>
-    
-    <!-- Menu XL -->
-    <hr class="mt-4 sm:mt-12">
-    <div class="desktop-menu">
-        <a href="/" class="menu-item">Startseite</a>
-        <a href="/task" class="menu-item">Aufgaben</a>
-        <a href="/members" class="menu-item">Behörde / Verwaltung</a>
-        <a href="/history" class="menu-item">Geschichte</a>
-        <a href="/regulations" class="menu-item">Reglemente</a>
-        <a href="/property" class="menu-item">Liegenschaften</a>
-        <a href="/forestry" class="menu-item">Forstbetrieb</a>
-    </div>
-    <hr>
 
-    <!-- Opened mobile menu -->
-    <div
-        v-motion
-        :initial="{ opacity: 0, x: 0 }"
-        :enter="{ opacity: 1, x: 0, scale: 1 }"
-        class="mobile-menu"
-        v-if="isOpen"
-        >
-            <a href="/" class="menu-item">Startseite</a>
-            <a href="/task" class="menu-item">Aufgaben</a>
-            <a href="/members" class="menu-item">Behörde / Verwaltung</a>
-            <a href="/history" class="menu-item">Geschichte</a>
-            <a href="/regulations" class="menu-item">Reglemente</a>
-            <a href="/property" class="menu-item">Liegenschaften</a>
-            <a href="/forestry" class="menu-item">Forstbetrieb</a>
-    </div>
+    <!-- Mobile menu button -->
+    <button class="mobile-menu-button" @click="toggle()">
+      <div class="space-y-1">
+        <span ref="upper" class="burger-line"></span>
+        <span ref="middle" class="burger-line"></span>
+        <span ref="lower" class="burger-line"></span>
+      </div>
+      <span class="font-medium ml-4">Menu</span>
+    </button>
+  </div>
+
+  <!-- Menu XL -->
+  <hr class="mt-4 sm:mt-12" />
+  <div class="desktop-menu">
+    <a href="/" class="menu-item">Startseite</a>
+    <a href="/task" class="menu-item">Aufgaben</a>
+    <a href="/members" class="menu-item">Verwaltung</a>
+    <a href="/history" class="menu-item">Geschichte</a>
+    <a href="/regulations" class="menu-item">Reglemente</a>
+    <a href="/property" class="menu-item">Liegenschaften</a>
+    <a href="/forestry" class="menu-item">Forstbetrieb</a>
+  </div>
+  <hr />
+
+  <!-- Opened mobile menu -->
+  <div
+    v-motion
+    :initial="{ opacity: 0, x: 0 }"
+    :enter="{ opacity: 1, x: 0, scale: 1 }"
+    class="mobile-menu"
+    v-if="isOpen"
+  >
+    <a href="/" class="menu-item">Startseite</a>
+    <a href="/task" class="menu-item">Aufgaben</a>
+    <a href="/members" class="menu-item">Verwaltung</a>
+    <a href="/history" class="menu-item">Geschichte</a>
+    <a href="/regulations" class="menu-item">Reglemente</a>
+    <a href="/property" class="menu-item">Liegenschaften</a>
+    <a href="/forestry" class="menu-item">Forstbetrieb</a>
+  </div>
 </template>
 
 <style scoped>
-    .upper-header {
-      @apply flex flex-col-reverse 2xl:flex-col justify-between text-gray-900 pt-8 sm:pt-16 mx-8 md:mx-16;
-    }
+.upper-header {
+  @apply flex flex-col-reverse 2xl:flex-col justify-between text-gray-900 pt-8 sm:pt-16 mx-8 md:mx-16;
+}
 
-    .header-title {
-      @apply flex-col lg:pl-8 mr-2 mt-2 md:mx-8 order-2;
-    }
+.header-title {
+  @apply flex-col lg:pl-8 mr-2 mt-2 md:mx-8 order-2;
+}
 
-    .header-title-main {
-      @apply font-bold text-3xl lg:text-5xl;
-    }
+.header-title-main {
+  @apply font-bold text-3xl lg:text-5xl;
+}
 
-    .header-title-secondary {
-      @apply lg:mt-4 sm:text-xl lg:text-2xl;
-    }
+.header-title-secondary {
+  @apply lg:mt-4 sm:text-xl lg:text-2xl;
+}
 
-    .header-text {
-      @apply flex max-md:justify-between;
-    }
+.header-text {
+  @apply flex max-md:justify-between;
+}
 
-    .header-logo {
-      @apply w-16 lg:w-24 order-3 md:order-1;
-    }
+.header-logo {
+  @apply w-16 lg:w-24 order-3 md:order-1;
+}
 
-    .burger-line {
-      @apply block h-0.5 w-6 bg-black;
-    }
+.burger-line {
+  @apply block h-0.5 w-6 bg-black;
+}
 
-    .desktop-menu {
-      @apply hidden 2xl:flex uppercase my-6 mr-16 justify-between;
-    }
+.desktop-menu {
+  @apply hidden 2xl:flex uppercase my-6 mr-16 justify-between;
+}
 
-    .mobile-menu-button {
-      @apply flex 2xl:hidden items-center z-10 mb-4 order-1 max-w-[8rem];
-    }
+.mobile-menu-button {
+  @apply flex 2xl:hidden items-center z-10 mb-4 order-1 max-w-[8rem];
+}
 
-    .mobile-menu {
-      @apply 2xl:hidden flex flex-col gap-16 uppercase fixed backdrop-blur-md bg-white/30 bottom-0 left-0 w-full h-screen items-center justify-center;
-    }
+.mobile-menu {
+  @apply 2xl:hidden flex flex-col gap-16 uppercase fixed backdrop-blur-md bg-white/30 bottom-0 left-0 w-full h-screen items-center justify-center;
+}
 
-    a:hover {
-        color: #00579F;
-    }
+a:hover {
+  color: #00579f;
+}
 
-    .menu-item {
-        @apply relative ml-16;
-    }
+.menu-item {
+  @apply relative ml-16 text-base;
+}
 </style>
 
 <script setup lang="ts">
-    import { ref } from "vue";
-    import { useMotions, useMotion } from '@vueuse/motion'
-    import axios from 'axios';
+import { ref } from "vue";
+import { useMotions, useMotion } from "@vueuse/motion";
 
-    /* Weather widget, eventually move me out to seperate component 
+/* Mobile menu, animations */
 
-    const API_KEY = "" // defineProps(['secret'])
-    const CITY_NAME = 'Därligen,CH';
-    const URL = `http://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&units=metric&appid=${API_KEY}`;
-    var response: any;
-    var weatherData: any;
+var isOpen = ref<boolean>(false);
+const upper = ref<HTMLElement>();
+const middle = ref<HTMLElement>();
+const lower = ref<HTMLElement>();
 
-    // figure out why try-catch doesn't work on me
-    if (API_KEY) {
-        response = await axios.get(URL);
-        weatherData = response.data;
-    }*/
-   
-    /* Mobile menu, animations */
+const toggle = () => {
+  isOpen.value = !isOpen.value;
+  rotateUpper();
+  rotateMiddle();
+  toggleLower();
+};
 
-    var isOpen = ref<boolean>(false);
-    const upper = ref<HTMLElement>()
-    const middle = ref<HTMLElement>()
-    const lower = ref<HTMLElement>()
+const { apply: applyUpper } = useMotion(upper, {
+  enter: {
+    rotateZ: 0,
+  },
+});
 
-    // TODO: Use css animations, this is odd
+const rotateUpper = async () => {
+  await applyUpper({
+    rotateZ: isOpen.value ? 45 : 0,
+    y: isOpen.value ? 5 : 0,
+    transition: {
+      type: "spring",
+      damping: 10,
+    },
+  });
+};
 
-    const toggle = () => {
-        
-        isOpen.value = !isOpen.value;
-        rotateUpper();
-        rotateMiddle();
-        toggleLower();
-    }
+const { apply: applyMiddle } = useMotion(middle, {
+  enter: {
+    rotateZ: 0,
+  },
+});
 
-    const { apply: applyUpper } = useMotion(upper, {
-        enter: {
-            rotateZ: 0
-        },
-    });
-        
-    const rotateUpper = async () => {
-       await applyUpper({
-          rotateZ: isOpen.value ? 45 : 0,
-          y: isOpen.value ? 5 : 0,
-          transition: {
-              type: 'spring',
-              damping: 10,
-           },
-        });
-    }
+const rotateMiddle = async () => {
+  await applyMiddle({
+    rotateZ: isOpen.value ? -45 : 0,
+    transition: {
+      type: "spring",
+      damping: 10,
+    },
+  });
+};
 
-    const { apply: applyMiddle } = useMotion(middle, {
-        enter: {
-            rotateZ: 0,
-        },
-    });
-        
-    const rotateMiddle = async () => {
-       await applyMiddle({
-          rotateZ: isOpen.value ? -45 : 0,
-          transition: {
-              type: 'spring',
-              damping: 10,
-           },
-        });
-    }
-    
-    const { apply: applyLower } = useMotion(lower, {
-        enter: {
-            opacity: 1,
-        },
-    });
+const { apply: applyLower } = useMotion(lower, {
+  enter: {
+    opacity: 1,
+  },
+});
 
-    const toggleLower = async () => {
-       await applyLower({
-          opacity: isOpen.value ? 0 : 1,
-        });
-    }
+const toggleLower = async () => {
+  await applyLower({
+    opacity: isOpen.value ? 0 : 1,
+  });
+};
 
-    useMotions();
+useMotions();
 </script>
--- src/components/map.vue
+++ src/components/map.vue
@@ -2,153 +2,127 @@
   <div ref="mapContainer" class="map-container"></div>
   <pre id="info"></pre>
 </template>
-  
-<script>
-  import mapboxgl from "mapbox-gl";
-  import MapboxDraw from "@mapbox/mapbox-gl-draw";
-  import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 
-  export default {
-    name: "MapboxMap",
-    props: {
-      center: {
-        type: Array,
-        default: () => [7.809985453040554, 46.658388077443476],
-      },
-      zoom: {
-        type: Number,
-        default: 14,
-      },
+<script>
+import mapboxgl from "mapbox-gl";
+import MapboxDraw from "@mapbox/mapbox-gl-draw";
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+
+export default {
+  name: "MapboxMap",
+  props: {
+    center: {
+      type: Array,
+      default: () => [7.809985453040554, 46.658388077443476],
     },
-    mounted() {
-      mapboxgl.accessToken = "pk.eyJ1IjoianVzdGluY2Vyb25pbyIsImEiOiJjbHoybGIwOGIweWpxMnNwZjF0Mmx1Z3V4In0.xPxdA5TUiG6v8xrAJtHWfQ";
-  
-      this.map = new mapboxgl.Map({
-        container: this.$refs.mapContainer,
-        style: "mapbox://styles/mapbox/outdoors-v12",
-        center: this.center,
-        zoom: this.zoom,
-      });
-  
-      this.map.addControl(new mapboxgl.NavigationControl());
-  
-      // Add a polygon (replace with the coordinates of your area)
-      this.map.on("load", () => {
-        this.map.addSource("polygon", {
-          type: "geojson",
-          data: {
-            type: "Feature",
-            geometry: {
-              type: "Polygon",
-              // Coordinates of the polygon (you can replace this with any shape)
-              coordinates: [
-                [
+    zoom: {
+      type: Number,
+      default: 14,
+    },
+  },
+  mounted() {
+    mapboxgl.accessToken = "";
+
+    this.map = new mapboxgl.Map({
+      container: this.$refs.mapContainer,
+      style: "mapbox://styles/mapbox/outdoors-v12",
+      center: this.center,
+      zoom: this.zoom,
+    });
+
+    this.map.addControl(new mapboxgl.NavigationControl());
+
+    // Add a polygon (replace with the coordinates of your area)
+    this.map.on("load", () => {
+      this.map.addSource("polygon", {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          geometry: {
+            type: "Polygon",
+            // Coordinates of the polygon (you can replace this with any shape)
+            coordinates: [
+              [
                 [7.809464504326286, 46.66090267196989],
                 [7.809428854499743, 46.660793682479664],
                 [7.8094888110254885, 46.66078144895209],
                 [7.809490431472398, 46.66079590675716],
                 [7.809542285765843, 46.6607881217858],
                 [7.809568212912012, 46.660888214192966],
-                [7.809464504326286, 46.66090267196989]
-                ],
+                [7.809464504326286, 46.66090267196989],
               ],
-            },
+            ],
           },
-        });
-  
-        /*
-        this.map.addLayer({
-          id: "polygon-layer",
-          type: "fill",
-          source: "polygon", // Refers to the source we just defined
-          layout: {},
-          paint: {
-            "fill-color": "#0080ff", // Color of the shape
-            "fill-opacity": 0.5, // Opacity of the shape
-          },
-        });*/
-  
-        // Add an outline around the polygon
-        /*
-        this.map.addLayer({
-          id: "polygon-outline",
-          type: "line",
-          source: "polygon",
-          layout: {},
-          paint: {
-            "line-color": "#000000",
-            "line-width": 2,
-          },
-        });
-        */
-      });
-
-      this.map.on('mousemove', (e) => {
-        document.getElementById('info').innerHTML =
-            // `e.point` is the x, y coordinates of the `mousemove` event
-            // relative to the top-left corner of the map.
-            JSON.stringify(e.point) +
-            '<br />' +
-            // `e.lngLat` is the longitude, latitude geographical position of the event.
-            JSON.stringify(e.lngLat.wrap());
-      });
-
-      const draw = new MapboxDraw({
-        displayControlsDefault: false,
-        // Select which mapbox-gl-draw control buttons to add to the map.
-        controls: {
-            polygon: true,
-            trash: true
         },
-        // Set mapbox-gl-draw to draw by default.
-        // The user does not have to click the polygon control button first.
-        defaultMode: 'draw_polygon'
       });
+    });
 
-      this.map.addControl(draw);
+    this.map.on("mousemove", (e) => {
+      document.getElementById("info").innerHTML =
+        // `e.point` is the x, y coordinates of the `mousemove` event
+        // relative to the top-left corner of the map.
+        JSON.stringify(e.point) +
+        "<br />" +
+        // `e.lngLat` is the longitude, latitude geographical position of the event.
+        JSON.stringify(e.lngLat.wrap());
+    });
 
-      this.map.on('draw.create', this.printPoints);
-    },
-    methods: {
-      printPoints(e) {
-        const features = e.features;
-        features.forEach((feature) => {
-          if (feature.geometry.type === "Polygon") {
-            console.log("Polygon coordinates:", feature.geometry.coordinates);
-          } else if (feature.geometry.type === "LineString") {
-            console.log("Line coordinates:", feature.geometry.coordinates);
-          } else if (feature.geometry.type === "Point") {
-            console.log("Point coordinates:", feature.geometry.coordinates);
-          }
-        });
+    const draw = new MapboxDraw({
+      displayControlsDefault: false,
+      // Select which mapbox-gl-draw control buttons to add to the map.
+      controls: {
+        polygon: true,
+        trash: true,
       },
-    },
-    beforeDestroy() {
-      if (this.map) {
-        this.map.remove();
-      }
-    },
-  };
-</script>
-  
-<style>
-  .map-container {
-    width: 100%;
-    height: 500px;
-  }
+      // Set mapbox-gl-draw to draw by default.
+      // The user does not have to click the polygon control button first.
+      defaultMode: "draw_polygon",
+    });
 
-  #info {
-        display: table;
-        position: relative;
-        margin: 0px auto;
-        word-wrap: anywhere;
-        white-space: pre-wrap;
-        padding: 10px;
-        border: none;
-        border-radius: 3px;
-        font-size: 12px;
-        text-align: center;
-        color: #222;
-        background: #fff;
+    this.map.addControl(draw);
+
+    this.map.on("draw.create", this.printPoints);
+  },
+  methods: {
+    printPoints(e) {
+      const features = e.features;
+      features.forEach((feature) => {
+        if (feature.geometry.type === "Polygon") {
+          console.log("Polygon coordinates:", feature.geometry.coordinates);
+        } else if (feature.geometry.type === "LineString") {
+          console.log("Line coordinates:", feature.geometry.coordinates);
+        } else if (feature.geometry.type === "Point") {
+          console.log("Point coordinates:", feature.geometry.coordinates);
+        }
+      });
+    },
+  },
+  beforeDestroy() {
+    if (this.map) {
+      this.map.remove();
     }
+  },
+};
+</script>
+
+<style>
+.map-container {
+  width: 100%;
+  height: 500px;
+}
+
+#info {
+  display: table;
+  position: relative;
+  margin: 0px auto;
+  word-wrap: anywhere;
+  white-space: pre-wrap;
+  padding: 10px;
+  border: none;
+  border-radius: 3px;
+  font-size: 12px;
+  text-align: center;
+  color: #222;
+  background: #fff;
+}
 </style>
